4d47b57f556bd976aefff878723ad5f55c6012a7
Caspian Maclean <caspianmaclean@gmail.com>
2022-02-23 11:36:37 +0800

    solved part 2

--- aoc-2021/day-1/puzzle-1-part-2.py
+++ aoc-2021/day-1/puzzle-1-part-2.py
@@ -6,17 +6,27 @@ print("accessing file: ", file_name)
 file = open(file_name, 'r')
 lines = file.readlines();
 
-first_line = True
+first_sum = True
 counted_increases = 0
 
+depth_list = []
+i = 0 #index into list
+
 for line in lines:
+    if i >= 3:
+        first_sum = False
+        previous_depth_sum = depth_sum
+
     depth = int(line)
-    if not first_line:
-        if depth > previous_depth:
+    depth_list.append(depth)
+    if i >= 2: # 3rd or later item, with zero-indexing
+        depth_sum = depth_list[i] + depth_list[i-1] + depth_list[i-2]
+
+    if not first_sum:
+        if depth_sum > previous_depth_sum:
             counted_increases += 1  
   
     # get ready for next iteration:
-    first_line = False
-    previous_depth = depth
+    i += 1
     
 print(counted_increases)
